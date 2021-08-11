9cb521e61e24c6e28635718e42d2b31289e36bb5
cindyhanae <cinde01@gmail.com>
2021-08-11 16:49:22 -0300

    update
    prevent scroll when menu open

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function toggleMenu(){
     document.querySelector('.bars').classList.toggle('active');
     document.querySelector('.menu-mobile').classList.toggle('active');
+    document.querySelector('body').classList.toggle('preventScroll');
 }
