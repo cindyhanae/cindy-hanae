80eeb9e59c378b58bfffbd4bebf53544d2478f9e
cindyhanae <cinde01@gmail.com>
2021-10-19 17:41:34 -0300

    update menu mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-function toggleMenu(){
-    document.querySelector('.bars').classList.toggle('active');
-    document.querySelector('.menu-mobile').classList.toggle('active');
-    document.querySelector('body').classList.toggle('preventScroll');
+const bars = document.querySelector('.js-bars')
+const menuMobile = document.querySelector('.js-menu-mobile')
+const menuItem = document.querySelectorAll('.js-menu-mobile ul li a')
+const contatoMenuMobile = document.querySelectorAll('.js-contato-menu a')
+
+contatoMenuMobile.forEach(function(item){
+    item.addEventListener('click', () => {
+        menuMobile.classList.remove('active')
+    })
+})
+
+
+menuItem.forEach(function(item){
+    item.addEventListener('click', () => {
+        menuMobile.classList.remove('active')
+    })
+})
+
+function openMenu(){
+    bars.classList.toggle('active')
+    menuMobile.classList.toggle('active')
 }
+
+bars.addEventListener('click', openMenu)
