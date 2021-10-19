c1298cd338855d1ff6da5e0b8b5580bf97bd9019
cindyhanae <cinde01@gmail.com>
2021-10-19 18:47:30 -0300

    smooth scroll

--- js/script.js
+++ js/script.js
@@ -5,28 +5,6 @@ const contatoMenuMobile = document.querySelectorAll('.js-contato-menu a')
 const body = document.querySelector('body')
 
 
-contatoMenuMobile.forEach(function(item){
-    item.addEventListener('click', () => {
-        menuMobile.classList.remove('active')
-    })
-
-    if(menuMobile.classList.contains('active') === false){
-        document.body.classList.remove('preventScroll')
-    }
-})
-
-
-menuItem.forEach(function(item){
-    item.addEventListener('click', () => {
-        bars.classList.remove('active')
-        menuMobile.classList.remove('active')
-
-        if(!menuMobile.classList.contains('active')){
-            body.style.overflow = 'auto'
-        }
-    })
-})
-
 function openMenu(){
     bars.classList.toggle('active')
     menuMobile.classList.toggle('active')
@@ -40,3 +18,33 @@ function openMenu(){
 }
 
 bars.addEventListener('click', openMenu)
+menuItem.forEach(function(item){
+    item.addEventListener('click', (event) => {
+        const href = event.currentTarget.getAttribute('href')
+        const section = document.querySelector(href)
+        const topSection = section.offsetTop
+
+        event.preventDefault()
+        bars.classList.remove('active')
+        menuMobile.classList.remove('active')
+
+        window.scrollTo({
+            top: topSection - 40,
+            behavior: 'smooth'
+        })
+
+
+        if(!menuMobile.classList.contains('active')){
+            body.style.overflow = 'auto'
+        }
+    })
+})
+contatoMenuMobile.forEach(function(item){
+    item.addEventListener('click', () => {
+        menuMobile.classList.remove('active')
+    })
+
+    if(menuMobile.classList.contains('active') === false){
+        document.body.classList.remove('preventScroll')
+    }
+})
